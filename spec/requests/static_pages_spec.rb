3ff50357e8ffde7331b3e857c8ec4ff2cb9f0c21
Inbal Lavie <inbal@simplee.com>
2018-01-10 17:18:15 +0200

    Improve tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -14,7 +14,11 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
     it "should have the title 'Contact'" do
       visit '/static_pages/contact'
       page.should have_selector('title',
-                        :text => "#{base_title} | Contact")
+                        :text => "#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Contact')
     end
   end
 
@@ -25,10 +29,15 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
       page.should have_selector('h1', :text => 'Sample App')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                        :text => "#{base_title} | Home")
+                        :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
@@ -42,7 +51,12 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "#{base_title} | Help")
+                        :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Help')
     end
   end
 
@@ -56,7 +70,12 @@ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
     it "should have the title 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('title',
-                    :text => "#{base_title} | About Us")
+                    :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| About Us')
     end
   end
 end
