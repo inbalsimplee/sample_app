334f400f403b3c1a11dd79cf06de7f287b961b1d
Inbal Lavie <inbal@simplee.com>
2018-01-11 14:04:43 +0200

    improve test #2

---
+++ spec/support/utilities.rb
@@ -0,0 +1,13 @@
+# Of course, this is essentially a duplicate of the helper in Listing 4.2 (./app/helpers/application_helper.rb), 
+# but having two independent methods allows us to catch any typos in the base title. 
+# This is dubious design, though, and a better (slightly more advanced) approach, 
+# which tests the original full_title helper directly, appears in the exercises (Section 5.6).
+
+def full_title(page_title)
+  base_title = "Ruby on Rails Tutorial Sample App"
+  if page_title.empty?
+    base_title
+  else
+    "#{base_title} | #{page_title}"
+  end
+end
