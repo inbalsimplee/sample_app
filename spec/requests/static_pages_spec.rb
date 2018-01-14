3759299f2a5253aa9136868cabb79d908fd06007
Inbal Lavie <inbal@simplee.com>
2018-01-14 11:18:21 +0200

    Added click_link method to the tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,48 +1,86 @@
 require 'spec_helper'
 
 describe "Static pages" do
-#let(:app_name) { "Sample App" }
-let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-  subject {page}
+
+ 
+ let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
+ subject {page}
+ it "Should have the right links on the layout" do
+    visit root_path
+
+    click_link "About"
+    should have_selector('title', text: full_title('About Us'))
+
+    click_link "Help"
+    should have_selector('title', text: full_title(''))
+
+    click_link "Contact"
+    should have_selector('title', text: full_title(''))
+
+    click_link "Home"
+    should have_selector('title', text: full_title(''))
+
+    click_link "Sign up now!"
+
+    click_link "sample app"
+
+
+ end
+
  describe "Contact page" do
+    let(:app_name) { 'Contact' }
+    let(:full_t) { '' }
+
     before {visit contact_path}
-    it { page.should have_selector('h1', text:  'Contact') }
+
+    it { page.should have_selector('h1', text: app_name) }
 
     it { should have_selector('title',
-                        text:  full_title('')) }
-    it { should have_selector('title', :text => '| Contact') }
+                        text:  full_title(full_t)) }
+    it { should have_selector('title', text: '| '+ app_name) }
   end
 
   
+ 
   describe "Home page" do
+    let(:app_name) { "Home" }
+    let(:full_t) { "" }
     before { visit root_path }
+ 
     it { should have_selector('h1', text: 'Sample App')}
 
-    it { should have_selector('title', text: full_title('')) }
+    it { should have_selector('title', text: full_title(full_t)) }
 
-    it { should_not have_selector('title', text: '| Home') }
+    it { should_not have_selector('title', text: '| ' + app_name) }
   end
 
   describe "Help page" do
+    
+    let(:app_name) { "Help" }
+    let(:full_t) { "" }
+
     before { visit help_path }
     it {
-      page.should have_selector('h1', :text => 'Help') }
+      page.should have_selector('h1', text: app_name) }
 
     it { should have_selector('title',
-                        text: full_title('')) }
+                        text: full_title(full_t)) }
 
-    it { should have_selector('title', :text => '| Help') }
+    it { should have_selector('title', text: '| ' + app_name) }
 
   end
 
   describe "About page" do
+    let(:app_name) { "About Us"}
+
     before { visit about_path }
-    it  { should have_selector('h1', :text => 'About Us') } 
+    it  { should have_selector('h1', text: app_name) } 
 
     it  { should have_selector('title',
                     text: "#{base_title}") }
 
-    it { should have_selector('title', text: '| About Us') }
+    it { should have_selector('title', text: '| ' + app_name) }
 
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -2,7 +2,7 @@
 # but having two independent methods allows us to catch any typos in the base title. 
 # This is dubious design, though, and a better (slightly more advanced) approach, 
 # which tests the original full_title helper directly, appears in the exercises (Section 5.6).
-
+include ApplicationHelper
 def full_title(page_title)
   base_title = "Ruby on Rails Tutorial Sample App"
   if page_title.empty?
