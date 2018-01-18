e377b13cd5a3d85e9afcb8037fad3eb1599bf6ef
Inbal Lavie <inbal@simplee.com>
2018-01-18 08:24:56 +0200

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ActiveRecord::Base
 
 
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
+
 
   validates :name, presence: true, length: { maximum: 50 }
 
@@ -26,4 +28,10 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6}
   validates :password_confirmation, presence: true 
   after_validation { self.errors.messages.delete(:password_digest) }
+
+  private
+
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,10 +26,14 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
 
   it { should be_valid}
-
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
   describe "When name is not valid" do
   	before { @user.name = ""}
   	it { should_not be_valid }
@@ -113,7 +117,7 @@ describe User do
     before { @user.password = @user.password_confirmation = "a" * 5 }
     it { should be_invalid }
   end
-  
+
   describe "email address with mixed case" do
     let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
 
